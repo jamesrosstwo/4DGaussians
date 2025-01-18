fix gradient flow for densification

gaussian_renderer/__init__.py
@@ -13,9 +13,6 @@ import torch
 import math
 from gsplat import rasterization
 from scene.gaussian_model import GaussianModel
-from utils.sh_utils import eval_sh
-from time import time as get_time
-
 
 def render(viewpoint_camera, pc: GaussianModel, pipe, bg_color: torch.Tensor, scaling_modifier=1.0, override_color=None,
            stage="fine", cam_type=None):
@@ -43,36 +40,14 @@ def render(viewpoint_camera, pc: GaussianModel, pipe, bg_color: torch.Tensor, sc
         device="cuda",
     )
 
-    # raster_settings = GaussianRasterizationSettings(
-    #     image_height=int(viewpoint_camera.image_height),
-    #     image_width=int(viewpoint_camera.image_width),
-    #     tanfovx=tanfovx,
-    #     tanfovy=tanfovy,
-    #     bg=bg_color,
-    #     scale_modifier=scaling_modifier,
-    #     viewmatrix=viewpoint_camera.world_view_transform.cuda(),
-    #     projmatrix=viewpoint_camera.full_proj_transform.cuda(),
-    #     sh_degree=pc.active_sh_degree,
-    #     campos=viewpoint_camera.camera_center.cuda(),
-    #     prefiltered=False,
-    #     debug=pipe.debug
-    # )
-
     time = torch.tensor(viewpoint_camera.time).to(means3D.device).repeat(means3D.shape[0], 1)
 
     means3D = pc.get_xyz
-    # add deformation to each points
-    # deformation = pc.get_deformation
     opacity = pc._opacity
     shs = pc.get_features
 
     scales = pc.get_scaling * scaling_modifier
     rotations = pc.get_rotation
-
-    # print(opacity.max())
-    # If precomputed colors are provided, use them. Otherwise, if it is desired to precompute colors
-    # from SHs in Python, do it. If not, then SH -> RGB conversion will be done by rasterizer.
-    # shs = None
 
     if override_color is not None:
         colors = override_color  # [N, 3]
@@ -81,14 +56,11 @@ def render(viewpoint_camera, pc: GaussianModel, pipe, bg_color: torch.Tensor, sc
         colors = pc.get_features  # [N, K, 3]
         sh_degree = pc.active_sh_degree
 
-    deformation_point = pc._deformation_table
     if "coarse" in stage:
-        means3D_final, scales_final, rotations_final, opacity_final, shs_final = means3D, scales, rotations, opacity, shs
+        means3D_final, scales_final, rotations_final, opacity_final, shs_final = means3D.to("cuda"), scales.to(
+            "cuda"), rotations.to("cuda"), opacity.to("cuda"), shs.to("cuda")
     elif "fine" in stage:
-        # time0 = get_time()
-        # means3D_deform, scales_deform, rotations_deform, opacity_deform = pc._deformation(means3D[deformation_point], scales[deformation_point],
-        #                                                                  rotations[deformation_point], opacity[deformation_point],
-        #                                                                  time[deformation_point])
+
         means3D_final, scales_final, rotations_final, opacity_final, shs_final = pc._deformation(means3D, scales,
                                                                                                  rotations, opacity,
                                                                                                  shs,
@@ -96,42 +68,35 @@ def render(viewpoint_camera, pc: GaussianModel, pipe, bg_color: torch.Tensor, sc
     else:
         raise NotImplementedError
 
-    # time2 = get_time()
-    # print("asset value:",time2-time1)
     scales_final = pc.scaling_activation(scales_final)
     rotations_final = pc.rotation_activation(rotations_final)
     opacity = pc.opacity_activation(opacity_final)
 
-    viewmat = viewpoint_camera.world_view_transform.transpose(0, 1)  # [4, 4]
+    viewmat = viewpoint_camera.world_view_transform.transpose(0, 1).to("cuda")  # [4, 4]
     render_colors, render_alphas, info = rasterization(
-        means=means3D_final.cuda(),  # [N, 3]
-        quats=rotations_final.cuda(),  # [N, 4]
-        scales=scales_final.cuda(),  # [N, 3]
-        opacities=opacity.squeeze(-1).cuda(),  # [N,]
-        colors=colors.cuda(),
-        viewmats=viewmat[None].cuda(),  # [1, 4, 4]
-        Ks=K[None].cuda(),  # [1, 3, 3]
-        backgrounds=bg_color[None].cuda(),
+        means=means3D_final,  # [N, 3]
+        quats=rotations_final,  # [N, 4]
+        scales=scales_final,  # [N, 3]
+        opacities=opacity.squeeze(-1),  # [N,]
+        colors=colors,
+        viewmats=viewmat[None],  # [1, 4, 4]
+        Ks=K[None],  # [1, 3, 3]
+        backgrounds=bg_color[None],
         width=int(viewpoint_camera.image_width),
         height=int(viewpoint_camera.image_height),
         packed=False,
         sh_degree=sh_degree,
     )
-
     rendered_image = render_colors[0].permute(2, 0, 1)
     radii = info["radii"].squeeze(0)  # [N,]
-    try:
-        info["means2d"].retain_grad()  # [1, N, 2]
-    except:
-        pass
 
-    # time4 = get_time()
-    # print("rasterization:",time4-time3)
-    # breakpoint()
+    info["means2d"].retain_grad()
+
     # Those Gaussians that were frustum culled or had a radius of 0 were not visible.
     # They will be excluded from value updates used in the splitting criteria.
-    return {"render": rendered_image,
-            "viewspace_points": info["means2d"],
-            "visibility_filter": radii > 0,
-            "radii": radii,
-            "depth": info["depths"]}
+    return {
+        "render": rendered_image,
+        "viewspace_points": info["means2d"],
+        "visibility_filter": radii > 0,
+        "radii": radii,
+        "depth": info["depths"].squeeze(0)}